e87d7155ecef40d3ee1797c53cb1e1bf7589348c
Cameron Lee <cal26@sfu.ca>
2024-03-01 12:41:33 -0800

    cleaned up api fetching skeleton structure

--- frontend/api/product.ts
+++ frontend/api/product.ts
@@ -12,34 +12,154 @@ export async function getProduct(productId: number) {
   return mock;
 }
 
-export async function getNewProducts() {
-  const mock = [
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 },
-    { id: 6 },
-    { id: 7 },
-    { id: 8 },
-    { id: 9 },
-    { id: 10 },
+export async function getNewProducts(): Promise<Product[]> {
+  const mock: Product[] = [
+    {
+      productId: 1,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      productId: 2,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      productId: 3,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      productId: 4,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      productId: 5,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      productId: 6,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      productId: 7,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      productId: 8,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      productId: 9,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      productId: 10,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
   ];
   return mock;
 }
 
-export async function getSaleProducts() {
-  const mock = [
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 },
-    { id: 6 },
-    { id: 7 },
-    { id: 8 },
-    { id: 9 },
-    { id: 10 },
+export async function getSaleProducts(): Promise<Product[]> {
+  const mock: Product[] = [
+    {
+      productId: 1,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      productId: 2,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      productId: 3,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      productId: 4,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      productId: 5,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      productId: 6,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      productId: 7,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      productId: 8,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      productId: 9,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      productId: 10,
+      name: "Wooden Stool",
+      imgSrc: "/images/wood-stool.jpg",
+      price: 15.2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
   ];
   return mock;
 }
--- frontend/components/ItemCard/ItemCard.tsx
+++ frontend/components/ItemCard/ItemCard.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
-import { useGetProduct } from "./api";
+import { Product } from "@/api/product.types";
 
 type ItemCardProps = {
-  productId: number;
+  isLoading: boolean;
+  error: Error | null;
+  product: Product;
 };
 
-function ItemCard({ productId }: ItemCardProps) {
+// TODO: conditionally render a loading card when isLoading is true
+function ItemCard({ isLoading, error, product }: ItemCardProps) {
   const router = useRouter();
-
-  const { isLoading, error, data } = useGetProduct(productId);
 
   return (
     <Card
@@ -18,22 +19,22 @@ function ItemCard({ productId }: ItemCardProps) {
       isPressable
       onClick={() => {
         if (!error && !isLoading) {
-          router.push(`/product/${productId}`);
+          router.push(`/product/${product.productId}`);
         }
       }}
     >
       <CardHeader className="py-2 px-4 flex-col items-start">
-        <h4 className="font-bold text-xl">{data && data.name}</h4>
+        <h4 className="font-bold text-xl">{product && product.name}</h4>
       </CardHeader>
       <CardBody className="overflow-visible py-2">
         <Image
           alt="Card background"
           className="object-cover rounded-xl pb-2"
-          src={data && data.imgSrc}
+          src={product && product.imgSrc}
           width={270}
         />
-        <p>${data && data.price.toFixed(2)}</p>
-        <p>{data && data.description}</p>
+        <p>${product && product.price.toFixed(2)}</p>
+        <p>{product && product.description}</p>
       </CardBody>
     </Card>
   );
