325be80cf632814aaf2930c9cfc8bee0f5efc750
Tawheed Sarker Aakash <bradwade7@gmail.com>
2024-03-13 07:22:30 -0700

    start

--- frontend/api/user.ts
+++
@@ -1,7 +0,0 @@
-import { CredentialResponse } from "@react-oauth/google";
-import { axios } from "./axios";
-import { UserTypes } from "./user.type";
-
-export async function getUserType(googleJWT: string): Promise<UserTypes> {
-	return UserTypes.Vendor;
-}
--- frontend/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	env: {
-		GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-		SECRET_KEY: process.env.SECRET_KEY,
-	},
-};
-
-export default nextConfig;
