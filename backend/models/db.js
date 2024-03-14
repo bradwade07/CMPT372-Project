2336cfad01d037534c579437369eec6f2250f350
Tawheed Sarker Aakash <bradwade7@gmail.com>
2024-03-13 17:38:11 -0700

    added init db helper function

--- backend/models/db.js
+++ backend/models/db.js
@@ -12,17 +12,19 @@ pool = new Pool({
 const helpers = {
   init: async function (req, res) {
     try {
-        await pool.query("CREATE TABLE IF NOT EXISTS products (pid SERIAL PRIMARY KEY, pname VARCHAR(255), pimgsrc VARCHAR(255), pdesc VARCHAR(255), padded bigINT, uemail VARCHAR(255), FOREIGN KEY (uemail) REFERENCES userInfo(uemail));");
-        await pool.query("CREATE TABLE IF NOT EXISTS productprice (pid INTEGER PRIMARY KEY, baseprice FLOAT, currentprice FLOAT, FOREIGN KEY (pid) REFERENCES products(pid));");
-        await pool.query("CREATE TABLE IF NOT EXISTS tags (tid SERIAL PRIMARY KEY, tname VARCHAR(255));");
-        await pool.query("CREATE TABLE IF NOT EXISTS producttags (pid INTEGER, tid INTEGER, PRIMARY KEY (pid, tid), FOREIGN KEY (pid) REFERENCES products(pid), FOREIGN KEY (tid) REFERENCES tags(tid));");
-        await pool.query("CREATE TABLE IF NOT EXISTS userInfo (uemail VARCHAR(255) PRIMARY KEY, uaddress TEXT);");
-        await pool.query("CREATE TABLE IF NOT EXISTS usercart (uemail VARCHAR(255), pid INTEGER, quantity INTEGER, PRIMARY KEY (uemail, pid), FOREIGN KEY (uemail) REFERENCES userInfo(uemail), FOREIGN KEY (pid) REFERENCES products(pid));");
-        await pool.query("CREATE TABLE IF NOT EXISTS usertypes (typeId SERIAL PRIMARY KEY, type VARCHAR(255));");
-        await pool.query("CREATE TABLE IF NOT EXISTS users (uemail VARCHAR(255), typeid INTEGER, PRIMARY KEY (uemail, typeid), FOREIGN KEY (uemail) REFERENCES userInfo(uemail), FOREIGN KEY (typeid) REFERENCES userTypes(typeid));");
-        await pool.query("CREATE TABLE IF NOT EXISTS userwishlist (uemail VARCHAR(255) PRIMARY KEY, pid INTEGER, FOREIGN KEY (uemail) REFERENCES userInfo(uemail), FOREIGN KEY (pid) REFERENCES products(pid));");
-        await pool.query("CREATE TABLE IF NOT EXISTS warehouses (wid SERIAL PRIMARY KEY, lat FLOAT, long FLOAT);");
-        await pool.query("CREATE TABLE IF NOT EXISTS warehousestock (wid INTEGER, pid INTEGER, PRIMARY KEY (wid, pid), stock INTEGER, FOREIGN KEY (wid) REFERENCES warehouses(wid), FOREIGN KEY (pid) REFERENCES products(pid));");
+        await pool.query("BEGIN")
+        await pool.query("CREATE TABLE IF NOT EXISTS product (product_id SERIAL PRIMARY KEY, product_name VARCHAR(255), product_imgsrc VARCHAR(255), product_description VARCHAR(255), product_date_added bigINT, user_email VARCHAR(255), FOREIGN KEY (user_email) REFERENCES userInfo(user_email));");
+        await pool.query("CREATE TABLE IF NOT EXISTS productprice (product_id INTEGER PRIMARY KEY, base_price FLOAT, current_price FLOAT, FOREIGN KEY (product_id) REFERENCES product(product_id));");
+        await pool.query("CREATE TABLE IF NOT EXISTS tag (tag_id SERIAL PRIMARY KEY, tag_name VARCHAR(255));");
+        await pool.query("CREATE TABLE IF NOT EXISTS producttags (product_id INTEGER, tag_id INTEGER, PRIMARY KEY (product_id, tag_id), FOREIGN KEY (product_id) REFERENCES product(product_id), FOREIGN KEY (tag_id) REFERENCES tag(tag_id));");
+        await pool.query("CREATE TABLE IF NOT EXISTS userInfo (user_email VARCHAR(255) PRIMARY KEY, user_address TEXT);");
+        await pool.query("CREATE TABLE IF NOT EXISTS usercart (user_email VARCHAR(255), product_id INTEGER, quantity INTEGER, PRIMARY KEY (user_email, product_id), FOREIGN KEY (user_email) REFERENCES userInfo(user_email), FOREIGN KEY (product_id) REFERENCES product(product_id));");
+        await pool.query("CREATE TABLE IF NOT EXISTS usertypes (type_id SERIAL PRIMARY KEY, type VARCHAR(255));");
+        await pool.query("CREATE TABLE IF NOT EXISTS users (user_email VARCHAR(255), type_id INTEGER, PRIMARY KEY (user_email, type_id), FOREIGN KEY (user_email) REFERENCES userInfo(user_email), FOREIGN KEY (type_id) REFERENCES userTypes(type_id));");
+        await pool.query("CREATE TABLE IF NOT EXISTS userwishlist (user_email VARCHAR(255), product_id INTEGER, PRIMARY KEY (user_email, product_id), FOREIGN KEY (user_email) REFERENCES userInfo(user_email), FOREIGN KEY (product_id) REFERENCES product(product_id));");
+        await pool.query("CREATE TABLE IF NOT EXISTS warehouse (warehouse_id SERIAL PRIMARY KEY, lat FLOAT, long FLOAT);");
+        await pool.query("CREATE TABLE IF NOT EXISTS warehousestock (warehouse_id INTEGER, product_id INTEGER, PRIMARY KEY (warehouse_id, product_id), stock INTEGER, FOREIGN KEY (warehouse_id) REFERENCES warehouse(warehouse_id), FOREIGN KEY (product_id) REFERENCES product(product_id));");
+        await pool.query("COMMIT")
     } catch (error) {
       await pool.query("ROLLBACK");
       console.error("Error creating tables:", error);
@@ -30,7 +32,7 @@ const helpers = {
     }
   },
   landingBackendFn: async function(req, res){
-    
+
   }
 };
 module.exports = { helpers };
