4a81a7be082d30e985d19e022b8db4f00caa3e13
Tawheed Sarker Aakash <bradwade7@gmail.com>
2024-03-14 21:07:26 -0700

    /backend has been modified

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,21 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get("/landingBackend", async (req, res) =>{
-    helpers.init(req, res);
-    helpers.landingBackendFn(req, res);
-
+app.post("/backend", async (req, res) =>{
+    try {
+        var response = helpers.init();
+        console.log("Tables created succesfully!");
+        res.status(200).send("Tables created succesfully!");
+      } catch (error) {
+        await pool.query("ROLLBACK");
+        console.error("Error creating tables:", error);
+        res.status(500).send("Error creating tables");
+      }
 })
+// app.get("/test", async (req, res) =>{
+//     helpers.getProductInfoByPid(req, res);
+    
+// })
+
+app.listen(port, "0.0.0.0");
+console.log(`Running on http://0.0.0.0:${port}`);
