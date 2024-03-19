32abf6d7e6fe9be066af093864c97ac97f13f2d1
Sunderinq <tegnoorg@sfu.ca>
2024-03-19 00:10:24 -0700

    added pool

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { helpers, connectWithConnector } = require('./models/db')
+const { helpers, pool } = require('./models/db')
 
 const port = 8080;
 
@@ -30,44 +30,6 @@ app.get("/landingBackend", async (req, res) =>{
 
 })
 
-// Route for adding a new user
-app.post('/addUser', async (req, res) => {
-
-  const { userEmail, userType } = req.body;
-  try {
-    await helpers.addUser(userEmail, userType);
-    res.status(201).json({ message: 'New user added successfully' });
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-// Route for updating user type
-app.patch('/updateUserType', async (req, res) => {
-
-  const { userEmail, userType } = req.body;
-  try {
-    await helpers.updateUserType(userEmail, userType);
-    res.status(200).json({ message: 'User type updated successfully' });
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-// Route for updating user address
-app.patch('/updateUserAddress', async (req, res) => {
-
-  const { userEmail, streetAddress, postalCode, city, province } = req.body;
-  try {
-    await helpers.updateUserAddress(userEmail, streetAddress, postalCode, city, province);
-    res.status(200).json({ message: 'User address updated successfully' });
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
 //test function
 app.get('/test', async (req, res) => {
   try {
@@ -80,29 +42,6 @@ app.get('/test', async (req, res) => {
 });
 
 
-app.delete('/cart', async (req, res) => {
-
-  const { userEmail, pid } = req.body;
-  try {
-    await helpers.removeFromCart(userEmail, pid);
-    res.status(200).json({ message: 'Item removed from shopping cart successfully' });
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-app.delete('/wishlist', async (req, res) => {
-
-  const { userEmail, pid } = req.body;
-  try {
-    await helpers.removeFromWishList(userEmail, pid);
-    res.status(200).json({ message: 'Item removed from wish list successfully' });
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
 
 
 app.listen(port, () => {
