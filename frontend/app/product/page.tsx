6a7ef81f6890c940918057ba4b78f585c49eeb8f
Sunderinq <tegnoorg@sfu.ca>
2024-03-28 23:34:10 -0700

    added quantity option

--- frontend/app/product/page.tsx
+++ frontend/app/product/page.tsx
@@ -1,5 +1,4 @@
-"use client"; // FIXME: shouldn't really be making a "page" component a client component, somehow refactor things later into client components so that this page doesn't have to have "use client"
-
+'use client';
 import { getProduct } from "@/api/product";
 import addToShoppingCart from "@/api/shoppingCart";
 import { addToWishlist } from "@/api/wishlist";
@@ -16,20 +15,18 @@ type SearchParams = {
 
 function page({ searchParams }: { searchParams: SearchParams }) {
   const router = useRouter();
-
-  const [selectedQuantity, setSelectedQuantity] = useState(1);
+  const [selectedQuantity, setSelectedQuantity] = useState(1); 
 
   const { isLoading, error, data } = useQuery({
     queryKey: ["Product", searchParams.product_id],
     queryFn: () => getProduct(searchParams.product_id),
   });
 
-  // queryClient and query invalidation used to force shopping cart and wishlist to refetch the updated contents
   const queryClient = useQueryClient();
 
   async function addItemToShoppingCart() {
     try {
-      await addToShoppingCart(searchParams.product_id, selectedQuantity);
+      await addToShoppingCart(searchParams.product_id, selectedQuantity); 
       queryClient.invalidateQueries({ queryKey: ["Shopping Cart"] });
     } catch (error) {
       router.push("/signin");
@@ -47,51 +44,59 @@ function page({ searchParams }: { searchParams: SearchParams }) {
     }
   }
 
+  const handleQuantityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    if (/^\d*$/.test(value)) {
+      setSelectedQuantity(parseInt(value));
+    }
+  };
+
   return (
     <>
       <TopNavbar />
       <main className="flex flex-col min-h-screen py-20">
-  <div className="container mx-auto flex flex-col md:flex-row">
-    <div className="flex flex-col justify-center items-center text-center md:w-2/5">
-      <div className="relative w-full h-96">
-        <img
-          src={"/images/grey.jpg"} // TODO: properly display image
-          alt="Product Image"
-          className="object-contain w-full h-full"
-        />
-      </div>
-      <div className="w-full mt-4">
-        <ImageSelector />
-      </div>
-      
-    </div>
-    <div className="flex flex-col justify-center items-center text-center md:w-3/5">
-      <p className="font-bold text-xl">{data?.product_name}</p>
-      <p className="text-lg">${data?.base_price}</p>
-      <p className="mb-8">{data?.product_description}</p>
-      <div className="flex flex-col gap-4 mb-8">
-        <Button className="bg-blue-500 hover:bg-blue-700 text-white" onClick={addItemToShoppingCart}>
-          ADD TO CART
-        </Button>
-        <Button onClick={addItemToWishlist}>
-          ADD TO WISHLIST
-        </Button>
-        <p>Quantity: 1</p>
-      </div>
-      <p className="mb-8">SPECIFICATIONS</p>
-    </div>
-  </div>
-  <div className="flex justify-center items-center text-center mt-8">
-    REVIEWS
-  </div>
-  <div className="flex justify-center items-center text-center mt-8">
-    WAREHOUSE MAP
-  </div>
-  <div className="flex justify-center items-center text-center mt-8">
-    ALSO LIKE THIS PRODUCT
-  </div>
-</main>
-
+        <div className="container mx-auto flex flex-col md:flex-row">
+          <div className="flex flex-col justify-center items-center text-center md:w-2/5">
+            <div className="relative w-full h-96">
+              <img
+                src={"/images/grey.jpg"} // TODO: properly display image
+                alt="Product Image"
+                className="object-contain w-full h-full"
+              />
+            </div>
+            <div className="w-full mt-4">
+              <ImageSelector />
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center text-center md:w-3/5">
+            <p className="font-bold text-xl">{data?.product_name}</p>
+            <p className="text-lg">${data?.base_price}</p>
+            <p className="mb-8">{data?.product_description}</p>
+            <div className="flex flex-col gap-4 mb-8">
+              <Button className="bg-blue-500 hover:bg-blue-700 text-white" onClick={addItemToShoppingCart}>
+                ADD TO CART
+              </Button>
+              <Button onClick={addItemToWishlist}>
+                ADD TO WISHLIST
+              </Button>
+              <div className="grid grid-cols-2 gap-3 place-items-center h-10">
+                <p>Quantity: </p>
+                <input  className="rounded-md max-w-10 bg-slate-100 ring-2 ring-blue-500" type="number" min="0" step="1" value={selectedQuantity} onChange={handleQuantityChange} />
+              </div>
+            </div>
+            <p className="mb-8">SPECIFICATIONS</p>
+          </div>
+        </div>
+        <div className="flex justify-center items-center text-center mt-8">
+          REVIEWS
+        </div>
+        <div className="flex justify-center items-center text-center mt-8">
+          WAREHOUSE MAP
+        </div>
+        <div className="flex justify-center items-center text-center mt-8">
+          ALSO LIKE THIS PRODUCT
+        </div>
+      </main>
     </>
   );
 }
