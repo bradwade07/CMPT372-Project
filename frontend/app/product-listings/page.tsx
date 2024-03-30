9b97f3df9bbbd6ba56423bbfee551c33aa57d60b
Sunderinq <tegnoorg@sfu.ca>
2024-03-29 22:54:32 -0700

    basic product listing page

--- frontend/app/product-listings/page.tsx
+++ frontend/app/product-listings/page.tsx
@@ -1,9 +1,55 @@
+'use client' //get rid of this
 import { TopNavbar } from "@/components/navbar";
 import Link from "next/link";
 import React from "react";
+import {Card, CardBody, CardFooter, Image} from "@nextui-org/react";
 
 function page() {
   // TODO: implement
+
+  const list = [
+    {
+      title: "Orange",
+      img: "/images/fruit-1.jpeg",
+      price: "$5.50",
+    },
+    {
+      title: "Tangerine",
+      img: "/images/fruit-2.jpeg",
+      price: "$3.00",
+    },
+    {
+      title: "Raspberry",
+      img: "/images/fruit-3.jpeg",
+      price: "$10.00",
+    },
+    {
+      title: "Lemon",
+      img: "/images/fruit-4.jpeg",
+      price: "$5.30",
+    },
+    {
+      title: "Avocado",
+      img: "/images/fruit-5.jpeg",
+      price: "$15.70",
+    },
+    {
+      title: "Lemon 2",
+      img: "/images/fruit-6.jpeg",
+      price: "$8.00",
+    },
+    {
+      title: "Banana",
+      img: "/images/fruit-7.jpeg",
+      price: "$7.50",
+    },
+    {
+      title: "Watermelon",
+      img: "/images/fruit-8.jpeg",
+      price: "$12.20",
+    },
+  ];
+  
   return (
     <>
       <TopNavbar />
@@ -18,6 +64,26 @@ function page() {
             CREATE NEW LISTING
           </Link>
         </div>
+        <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+      {list.map((item, index) => (
+        <Card shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")}>
+          <CardBody className="overflow-visible p-0">
+            <Image
+              shadow="sm"
+              radius="lg"
+              width="100%"
+              alt={item.title}
+              className="w-full object-cover h-[140px]"
+              src={item.img}
+            />
+          </CardBody>
+          <CardFooter className="text-small justify-between">
+            <b>{item.title}</b>
+            <p className="text-default-500">{item.price}</p>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
       </main>
     </>
   );
