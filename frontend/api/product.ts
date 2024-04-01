b540e2ccafb99f68a79e15119d7313b6a15fbf21
Cameron Lee <cal26@sfu.ca>
2024-03-31 19:44:03 -0700

    getProduct function returns full info for a product

--- frontend/api/product.ts
+++ frontend/api/product.ts
@@ -1,13 +1,15 @@
-import { Product, ProductListingCreation } from "./product.types";
+import { Product, ProductFull, ProductListingCreation } from "./product.types";
 import { axios } from "./axios";
 import { isAxiosError } from "axios";
 import { FiltersType, filtersToQueryString } from "./filters.types";
 import { getSessionUserEmail } from "@/app/auth";
 
 // given a product's id, returns all that product's info
-export async function getProduct(product_id: number): Promise<Product | null> {
+export async function getProduct(
+  product_id: number,
+): Promise<ProductFull | null> {
   try {
-    let response = await axios.get<Product[]>(`/getProduct/${product_id}`);
+    let response = await axios.get<ProductFull[]>(`/getProduct/${product_id}`);
 
     return response.data[0];
   } catch (error) {
