2dde523ce28b1b717af01d06083c2f674b7827bc
Sunderinq <tegnoorg@sfu.ca>
2024-04-03 02:41:41 -0700

    got the image selector working. just need to figure out how to update the actual main image when image is changed

--- frontend/app/product/page.tsx
+++ frontend/app/product/page.tsx
@@ -108,15 +108,11 @@ function page({ searchParams }: { searchParams: SearchParams }) {
       <main className="flex flex-col min-h-screen py-20">
         <div className="container mx-auto flex flex-col md:flex-row">
           <div className="flex flex-col justify-center items-center text-center md:w-2/5">
-            <div className="relative w-full h-96">
-              <img
-                src={`data:image/jpeg;base64, ${data?.product_main_img}`}
-                alt="Product Image"
-                className="object-contain w-full h-full"
-              />
-            </div>
             <div className="w-full mt-4">
-              <ImageSelector />
+            {data ? (
+                <ImageSelector tags={data.tags} additional_img={data.additional_img} product_id={data.product_date_added} product_name={data.product_name} product_description={data.product_description} base_price={data.base_price} current_price={data.current_price} product_date_added={data.product_date_added} product_main_img={data.product_main_img} />
+              ) : (<div></div>
+              )}
             </div>
           </div>
           <div className="flex flex-col justify-center items-center text-center md:w-3/5">
--- frontend/components/ImageSelector/ImageSelector.tsx
+++ frontend/components/ImageSelector/ImageSelector.tsx
@@ -1,32 +1,60 @@
-import { useState } from "react";
-import Image from "next/image";
+import { MouseEventHandler, useState } from "react";
+import { Image, Button } from "@nextui-org/react";
+import { ProductFull } from "@/api/product.types"
+import { IoReload } from "react-icons/io5"
 
-export default function ImageSelector() {
-  const [selectedImage, setSelectedImage] = useState(null);
+export default function ImageSelector(imageData: ProductFull) {
+  const [selectedImage, setSelectedImage] = useState("");
+  const [pictures, setPictures] = useState([imageData.product_main_img]);
+  const [firstTimeRender, setFirstTimeRender] = useState(true);
+if(firstTimeRender)
+{  
+  if(imageData.additional_img){
+    let list = pictures;
+    for(let i = 0; i < imageData.additional_img.length; i++ ){
+      list[i+1] = imageData.additional_img[i];
+    }
+    setPictures(list);
+  }
+  
+  setFirstTimeRender(false);
+  setSelectedImage(pictures[0]);
+}
 
-  return (
+const handleSelected = (index: number): MouseEventHandler<HTMLImageElement> => {
+  return (event:any) => {
+    setSelectedImage(pictures[index]);
+  };
+};
+
+  return (<>
+    <div className="relative w-full h-96">
+              <Image
+                isZoomed
+                width={1000}
+                height={1000}
+                src={`data:image/jpeg;base64, ${selectedImage}`}
+                alt="Product Image"
+                className="object-contain"
+              />
+            </div>
     <div className="w-full mt-4">
       <h2>IMAGE SELECTOR</h2>
       <div className="flex justify-between mt-2">
-        {[1, 2, 3, 4, 5].map((index) => (
+        {pictures.map((index:any) => (
           <Image
+            onClick={handleSelected(index)}
+            isZoomed
             key={index}
             width={100}
             height={100}
-            src={`/images/grey.jpg`}
+            src={`data:image/jpeg;base64, ${index}`}
             alt={`grey.jpg`}
-            className={`cursor-pointer ${selectedImage === `/images/grey.jpg` ? "border-2 border-blue-500" : ""}`} ////images/image${index}.jpg
+            className={`cursor-pointer ${selectedImage === `${index}` ? "border-2 border-blue-500" : ""}`} ////images/image${index}.jpg
           />
         ))}
       </div>
-      <div className="mt-2">
-        Selected Image:{" "}
-        {selectedImage ? (
-          <img src={selectedImage} alt="Selected Image" className="w-20 h-20" />
-        ) : (
-          "None"
-        )}
-      </div>
     </div>
+    </>
   );
 }
