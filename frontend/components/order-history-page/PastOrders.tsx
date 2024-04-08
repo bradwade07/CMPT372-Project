4b1960ef02cf02a253c6e3630f12380aa7a26bf2
Cameron Lee <cal26@sfu.ca>
2024-04-07 17:04:00 -0700

    order history styling

--- frontend/components/order-history-page/PastOrders.tsx
+++ frontend/components/order-history-page/PastOrders.tsx
@@ -13,15 +13,24 @@ export function PastOrders() {
 
   return (
     <div className="mx-4">
-      <div className="max-h-[80vh] overflow-y-auto px-2">
-        {data?.map((item) => (
-          <div className="mb-10" key={item.order_id}>
+      <div className="max-h-[80vh] overflow-y-auto px-2 mt-4">
+        {data?.map((item, index) => (
+          <div
+            className="flex flex-col mx-auto mb-10 w-2/3"
+            key={item.order_id}
+          >
+            <p className="text-center text-2xl">Order {index + 1}</p>
             {item.products.map((product) => (
               <span key={product.product_id}>
                 <ProductEntry productEntry={product} />
               </span>
             ))}
-            <p>Order Date: {new Date(item.order_date * 1000).toUTCString()}</p>
+            <p>
+              Order Date:{" "}
+              {new Date(item.order_date * 1000).toLocaleString("en-US", {
+                timeZone: "America/Los_Angeles",
+              })}
+            </p>
           </div>
         ))}
       </div>
